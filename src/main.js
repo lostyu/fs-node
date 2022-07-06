5fd94e4fc074abc4ff512c9f46b4ee5fa26efa4b
tonysoul <tonysoul@foxmail.com>
2022-07-06 15:31:08 +0800

    创建web服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
-let greeting = '你好!';
-console.log(greeting);
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+  res.write('hello');
+  res.end();
+});
+
+server.listen(3000, ()=>{
+  console.log('🚀服务已启动');
+});
