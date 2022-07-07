af4aa5f01b4c5013bfb5d5e13c8ba53d8d3d4658
tonysoul <tonysoul@foxmail.com>
2022-07-07 11:52:14 +0800

    web server

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-  res.write('hello');
+  const data = {
+    "id": 1,
+    "name": "tony"
+  }
+
+  
+  const jsonData = JSON.stringify(data);
+
+  res.writeHead(200, {
+    'Content-type': 'application/json; charset=utf-8'
+  });
+
+  res.write(jsonData);
   res.end();
+
 });
 
 server.listen(3000, ()=>{
