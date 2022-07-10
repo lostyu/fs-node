cb8b7d7ccda3dd640fdf116e82e2d42daf012f79
tonysoul <tonysoul@foxmail.com>
2022-07-10 10:55:33 +0800

    add user register model

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import postRouter from "../post/post.router";
+import userRouter from "../user/user.router";
 import { defaultErrorHandler } from "./app.middleware";
 
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 // 路由
-app.use(postRouter);
+app.use(postRouter, userRouter);
 
 // 处理异常
 app.use(defaultErrorHandler);
