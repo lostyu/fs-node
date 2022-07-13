c50c999aaecd21e64492d677242df991f057be39
tonysoul <tonysoul@foxmail.com>
2022-07-13 15:49:16 +0800

    jimp获取图像信息

--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import multer from "multer";
+import Jimp from "jimp";
 
 // 创建multer
 const fileUpload = multer({
@@ -8,3 +9,30 @@ const fileUpload = multer({
 
 // file拦截器
 export const fileInterceptor = fileUpload.single("file");
+
+interface MulterRequest extends Request {
+  file: any;
+}
+/**
+ * 图像处理
+ */
+export const fileProcessor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // 获取图像路径
+  const { path } = (req as MulterRequest).file;
+
+  let image: Jimp;
+
+  try {
+    image = await Jimp.read(path);
+
+    console.log(image);
+  } catch (error) {
+    next(error);
+  }
+
+  next();
+};
--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import * as fileController from "./file.controller";
 import { authGuard } from "../auth/auth.middleware";
-import { fileInterceptor } from "./file.middleware";
+import { fileInterceptor, fileProcessor } from "./file.middleware";
 
 const router = Router();
 
-router.post("/files", authGuard, fileInterceptor, fileController.store);
+router.post(
+  "/files",
+  authGuard,
+  fileInterceptor,
+  fileProcessor,
+  fileController.store
+);
 
 router.get("/files/:fileId/serve", fileController.serve);
 
