df26dbfe2242a07ac1442f6679e3411411b8dfbc
tonysoul <tonysoul@foxmail.com>
2022-07-14 15:05:11 +0800

    add tag service

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,1 +1,15 @@
 import { connection } from "../app/database/mysql";
+import { TagModel } from "./tag.model";
+
+/**
+ * 创建标签
+ */
+export const createTag = async (tag: TagModel) => {
+  const statement = `
+    INSERT INTO tag SET ?
+  `;
+
+  const [data] = await connection.promise().query(statement, tag);
+
+  return data as any;
+};
