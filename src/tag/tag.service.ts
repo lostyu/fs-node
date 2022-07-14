0dc5d4959dd485a43b1533501d9aa9435b839652
tonysoul <tonysoul@foxmail.com>
2022-07-14 15:08:24 +0800

    getTagByName

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,3 +1,4 @@
+import { RowDataPacket } from "mysql2";
 import { connection } from "../app/database/mysql";
 import { TagModel } from "./tag.model";
 
@@ -12,4 +13,15 @@ export const createTag = async (tag: TagModel) => {
   const [data] = await connection.promise().query(statement, tag);
 
   return data as any;
+};
+
+/**
+ * 按名字查找标签
+ */
+export const getTagByName = async (tagName: string) => {
+  const statement = `SELECT id, name FROM tag WHERE name = ?`;
+
+  const [data] = await connection.promise().query(statement, tagName);
+
+  return (data as RowDataPacket)[0];
 };
