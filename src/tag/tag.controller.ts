4c3f5efb090e2a662c2c87c1d5bb8b70effc2759
tonysoul <tonysoul@foxmail.com>
2022-07-14 15:16:18 +0800

    定义创建标签用的接口

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,1 +1,27 @@
 import { Request, Response, NextFunction } from "express";
+import { createTag, getTagByName } from "./tag.service";
+
+/**
+ * 创建标签
+ */
+export const store = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name } = req.body;
+
+  try {
+    // 查询名字是否重复
+    const tag = await getTagByName(name);
+    if (tag) {
+      throw new Error("TAG_ALREADY_EXIST");
+    }
+
+    const data = await createTag({ name });
+
+    res.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/tag/tag.router.ts
+++ src/tag/tag.router.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { authGuard } from "../auth/auth.middleware";
 import * as tagController from "./tag.controller";
 
 const router = Router();
+
+router.post("/tags", authGuard, tagController.store);
 
 export default router;
