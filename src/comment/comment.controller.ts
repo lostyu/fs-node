f372063332cc58a00b70a8d88be31e8440261dc4
tonysoul <tonysoul@foxmail.com>
2022-07-15 15:18:25 +0800

    评论接口

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,1 +1,28 @@
 import { Request, Response, NextFunction } from "express";
+import { createComment } from "./comment.service";
+
+/**
+ * 发表评论
+ */
+export const store = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // 准备数据
+  const { id: userId } = req.user;
+  const { content, postId } = req.body;
+
+  const comment = {
+    userId,
+    content,
+    postId,
+  };
+
+  try {
+    const data = await createComment(comment);
+    res.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/comment/comment.router.ts
+++ src/comment/comment.router.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { authGuard } from "../auth/auth.middleware";
 import * as commentController from "./comment.controller";
 
 const router = Router();
+
+router.post("/comments", authGuard, commentController.store);
 
 export default router;
