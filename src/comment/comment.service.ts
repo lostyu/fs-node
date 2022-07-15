f1fa84cc72d6591341a847729602ef583cd7f82b
tonysoul <tonysoul@foxmail.com>
2022-07-15 15:13:28 +0800

    commnet service

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,1 +1,15 @@
 import { connection } from "../app/database/mysql";
+import { CommentModel } from "./comment.model";
+
+/**
+ * 创建评论
+ */
+export const createComment = async (comment: CommentModel) => {
+  const statement = `
+    INSERT INTO comment SET ?
+  `;
+
+  const [data] = await connection.promise().query(statement, comment);
+
+  return data;
+};
