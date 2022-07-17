4f9052b4a83c715c3bcf947a89d60acd579a6500
tonysoul <tonysoul@foxmail.com>
2022-07-17 11:45:58 +0800

    定义保存头像数据功能

--- src/avatar/avatar.service.ts
+++ src/avatar/avatar.service.ts
@@ -1,1 +1,15 @@
 import { connection } from "../app/database/mysql";
+import { AvatarModel } from "./avatar.model";
+
+/**
+ * 保存头像信息
+ */
+export const createAvatar = async (avatar: AvatarModel) => {
+  const statement = `
+    INSERT INTO avatar SET ?
+  `;
+
+  const data = await connection.promise().query(statement, avatar);
+
+  return data;
+};
