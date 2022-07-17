2a08e2c872fb76c566fae60c08ae67a096821ce8
tonysoul <tonysoul@foxmail.com>
2022-07-17 15:27:16 +0800

    定义点赞内容接口

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,1 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import { createUserLikePost } from "./like.service";
+
+/**
+ * 点赞内容
+ */
+export const store = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id: userId } = req.user;
+  const { postId } = req.params;
+
+  try {
+    const data = await createUserLikePost(Number(userId), Number(postId));
+    res.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/like/like.router.ts
+++ src/like/like.router.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { authGuard } from "../auth/auth.middleware";
 import * as likeController from "./like.controller";
 
 const router = Router();
+
+router.post("/posts/:postId/like", authGuard, likeController.store);
 
 export default router;
