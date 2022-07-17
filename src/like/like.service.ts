e4b78d975319b64c1b33b85eafcca7f37c4508d5
tonysoul <tonysoul@foxmail.com>
2022-07-17 15:22:16 +0800

    定义保存点赞内容的功能

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,1 +1,16 @@
 import { connection } from "../app/database/mysql";
+
+/**
+ * 保存用户点赞内容
+ */
+export const createUserLikePost = async (userId: number, postId: number) => {
+  const statement = `
+    INSERT INTO 
+      user_like_post (userId, postId)
+    VALUES (?, ?)
+  `;
+
+  const [data] = await connection.promise().query(statement, [userId, postId]);
+
+  return data;
+};
