f9e48d4eda0c6b27ae44953b5a657dafa0b50b12
tonysoul <tonysoul@foxmail.com>
2022-07-17 18:13:44 +0800

    取消用户点赞功能

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -14,3 +14,16 @@ export const createUserLikePost = async (userId: number, postId: number) => {
 
   return data;
 };
+
+/**
+ * 取消用户点赞
+ */
+export const deleteUserLikePost = async (userId: number, postId: number) => {
+  const statement = `
+    DELETE FROM user_like_post WHERE userId=? AND postId=?
+  `;
+
+  const [data] = await connection.promise().query(statement, [userId, postId]);
+
+  return data;
+};
