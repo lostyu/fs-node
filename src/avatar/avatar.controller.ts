f72a52b0c0b1bb90fde95bae0564e4e5e48ed820
tonysoul <tonysoul@foxmail.com>
2022-07-17 11:10:36 +0800

    用户头像上传接口定义

--- src/avatar/avatar.controller.ts
+++ src/avatar/avatar.controller.ts
@@ -1,1 +1,12 @@
 import { Request, Response, NextFunction } from "express";
+
+/**
+ * 头像上传
+ */
+export const store = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.sendStatus(200);
+};
--- src/avatar/avatar.router.ts
+++ src/avatar/avatar.router.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { authGuard } from "../auth/auth.middleware";
 import * as avatarController from "./avatar.controller";
+import { avatarInterceptor } from "./avatar.middleware";
 
 const router = Router();
+router.post("/avatar", authGuard, avatarInterceptor, avatarController.store);
 
 export default router;
