7b4128755750030f34b8b087aed7cbd3ec215495
tonysoul <tonysoul@foxmail.com>
2022-07-17 11:06:50 +0800

    创建上传头像中间件

--- src/avatar/avatar.middleware.ts
+++ src/avatar/avatar.middleware.ts
@@ -1,1 +1,15 @@
 import { Request, Response, NextFunction } from "express";
+import multer from "multer";
+import { fileFilter } from "../file/file.middleware";
+
+// 文件过滤器
+const avatarUploadFilter = fileFilter(["image/png", "image/jpg", "image/jpeg"]);
+
+// multer
+const avatarUpload = multer({
+  dest: "uploads/avatar",
+  fileFilter: avatarUploadFilter,
+});
+
+// interceptor
+export const avatarInterceptor = avatarUpload.single("avatar");
