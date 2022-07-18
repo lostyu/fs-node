7c7c775a549df3039ca603fd1a70d2f84ea02453
tonysoul <tonysoul@foxmail.com>
2022-07-18 12:19:40 +0800

    sql代码片段

---
+++ src/comment/comment.provider.ts
@@ -0,0 +1,25 @@
+export const sqlFragment = {
+  user: `
+    JSON_OBJECT(
+      'id', user.id,
+      'name', user.name,
+      'avatar', IF(COUNT(avatar.id), 1, NULL)
+    ) AS user
+  `,
+  post: `
+    JSON_OBJECT(
+      'id', post.id,
+      'title', post.title
+    ) AS post
+  `,
+  leftJoinUser: `
+    LEFT JOIN user
+      ON user.id = comment.userId
+    LEFT JOIN avatar
+      ON avatar.userId = user.id
+  `,
+  leftJoinPost: `
+    LEFT JOIN post
+      ON post.id = comment.postId
+  `,
+};
