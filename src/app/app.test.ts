f70c1d5913d8b69b5304ff0565d15e72aa732012
tonysoul <tonysoul@foxmail.com>
2022-07-19 15:43:45 +0800

    创建接口测试

--- src/app/app.test.ts
+++ src/app/app.test.ts
@@ -1,4 +1,7 @@
 import { greet } from "./playground/demo";
+import request from "supertest";
+import app from "../app";
+import { connection } from "../app/database/mysql";
 
 /**
  * 单元测试
@@ -10,5 +13,33 @@ describe("演示单元测试", () => {
     const greeting = greet("Tony");
     // 断言
     expect(greeting).toBe("你好，Tony");
+  });
+});
+
+/**
+ * 接口测试
+ */
+describe("演示接口测试", () => {
+  afterAll(async () => {
+    // 断开连接
+    connection.end();
+  });
+
+  test("测试 GET /", async () => {
+    // 请求接口
+    const response = await request(app).get("/");
+
+    // 断言
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ title: "小白兔的开发之路" });
+  });
+
+  test("测试 POST /echo", async () => {
+    const response = await request(app).post("/echo").send({
+      message: "你好~",
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual({ message: "你好~" });
   });
 });
