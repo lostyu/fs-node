772657c0592c2e134c76d5ff5e511206bf6a58e8
tonysoul <tonysoul@foxmail.com>
2022-07-20 11:38:42 +0800

    测试创建用户接口

--- src/user/user.test.ts
+++ src/user/user.test.ts
@@ -27,3 +27,36 @@ afterAll(async () => {
   // 断开连接
   connection.end();
 });
+
+/**
+ * 创建用户
+ */
+describe("测试创建用户接口", () => {
+  test("创建用户时必须提供用户名", async () => {
+    const response = await request(app).post("/users").send({
+      password: testUser.password,
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: "请提供用户名" });
+  });
+
+  test("创建用户时必须提供密码", async () => {
+    const response = await request(app).post("/users").send({
+      name: testUser.name,
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: "请提供密码" });
+  });
+
+  test("成功创建用户以后，响应状态码应该是 201", async () => {
+    const response = await request(app).post("/users").send(testUser);
+
+    testUserCreated = await getUserById(response.body.insertId, {
+      password: true,
+    });
+
+    expect(response.status).toBe(201);
+  });
+});
